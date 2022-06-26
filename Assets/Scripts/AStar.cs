1db6b53065eb911d1e6964ff435b297006a02c62
NukeyFox <johananmahendran@gmail.com>
2022-06-26 23:02:57 +0100

    A* wih FastPriorityQueue

--- Assets/Scripts/AStar.cs
+++ Assets/Scripts/AStar.cs
@@ -1,6 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+using Priority_Queue;
 
 
 /* Usage
@@ -20,64 +18,87 @@ namespace AStarFunctions
 {
   public class AStar
   {
+    public class Pair : FastPriorityQueueNode{
+        public int Item1;
+        public int Item2;
+        public Tuple<int,int> tuple;
+        public Pair(int a, int b){Item1 = a; Item2 = b; tuple = new Tuple<int,int>(a,b);}
 
+        public override string ToString(){
+          return tuple.ToString();
+        }
 
+      public override bool Equals(object? obj)
+      {
+        if (obj is Pair){
+          Pair p =(Pair) obj;
+          return p.tuple.Equals(this.tuple);
+        }
+        return false;
+      }
+
+      public override int GetHashCode()
+      {
+        return tuple.GetHashCode();
+      }
+    }
 
     /////////////// The actual A* search stuff /////////////////////
 
-    private List<Tuple<int, int>> build_path(Tuple<int, int> source, Tuple<int, int> target, Dictionary<Tuple<int, int>, Tuple<int, int>> backtrack)
+    private List<Tuple<int,int>> build_path(Pair source, Pair target, Dictionary<Pair, Pair> backtrack)
     {
       //Back  tracks  to  get  the  path  from  source  to  the  target
-      Tuple<int, int> curr = target;
-      List<Tuple<int, int>> arr = new List<Tuple<int, int>>();
-      arr.Add(target);
+      Pair curr = target;
+      List<Tuple<int,int>> arr = new List<Tuple<int,int>>();
+      arr.Add(curr.tuple);
 
       while (curr != source)
       {
         curr = backtrack[curr];
-        arr.Insert(0, curr);
+        arr.Insert(0, curr.tuple);
       }
       return arr;
     }
 
-    public double distance(Tuple<int, int> p, Tuple<int, int> q)
+    public float distance(Pair p, Pair q)
     {
-      return Math.Sqrt(Math.Pow(p.Item1 - q.Item1, 2) + Math.Pow(p.Item2 - q.Item2, 2));
+      return (float) Math.Sqrt(Math.Pow(p.Item1 - q.Item1, 2) + Math.Pow(p.Item2 - q.Item2, 2));
     }
 
 
     //Call this function to find a shortest path from the point (startx, starty) to (endx, endy) on terrain terrain
-    public List<Tuple<int, int>> pathTo(int startx, int starty, int endx, int endy, int[,] terrain)
+    public List<Tuple<int,int>> pathTo(int startx, int starty, int endx, int endy, int[,] terrain)
     {
 
-      Tuple<int, int> source = new Tuple<int, int>(startx, starty);
-      Tuple<int, int> target = new Tuple<int, int>(endx, endy);
+      Pair source = new Pair(startx, starty);
+      Pair target = new Pair(endx, endy);
 
       int map_width = terrain.GetLength(0);
       int map_height = terrain.GetLength(1);
 
       //Keep  track  of  which  nodes  seen  for  backtracking
-      Dictionary<Tuple<int, int>, Tuple<int, int>> backtrack = new Dictionary<Tuple<int, int>, Tuple<int, int>>();
+      Dictionary<Pair, Pair> backtrack = new Dictionary<Pair, Pair>();
 
       //Keeps  track  of  the  pathcost  of  each  node
-      Dictionary<Tuple<int, int>, int> pathcost = new Dictionary<Tuple<int, int>, int>();
+      Dictionary<Pair, int> pathcost = new Dictionary<Pair, int>();
 
       //A  queue  for  sorting  nodes  using  the  A*  cost
-      PriorityQueue<Tuple<int, int>, double> queue = new PriorityQueue<Tuple<int, int>, double>();
+      Priority_Queue.FastPriorityQueue<Pair> queue = new Priority_Queue.FastPriorityQueue<Pair>(map_width * map_height);
       pathcost[source] = 0;
       queue.Enqueue(source, distance(source, target));
 
-
-      void Enqueue(int x, int y, Tuple<int, int> node)
+      //int count = 0;
+      void Enqueue(int x, int y, Pair node)
       {
+        //count++;
         if (x >= 0 && x < map_width && y >= 0 && y < map_height)
         {
-          Tuple<int, int> p = new Tuple<int, int>(x, y);
+          Pair p = new Pair(x, y);
           
           if (terrain[x,y] >= 0)
           {
             int pc =  pathcost[node] + terrain[x,y];
-            double cost = pc + distance(p, target); 
+            float cost = pc + distance(p, target); 
             if (!pathcost.ContainsKey(p) || pc < pathcost[p])
             {
               pathcost[p] = pc;
@@ -93,12 +114,13 @@ namespace AStarFunctions
       {
         //queue.printQueue();
         //Console.WriteLine(queue);
-        Tuple<int, int> node = queue.Dequeue();
+        Pair node = queue.Dequeue();
         //Console.WriteLine(node);
 
         //If  target  is  reached  then  backtrack  and  return  the  path  from  source  to  target
         if (node.Equals(target))
         {
+         // Console.WriteLine(count);
           return build_path(source, node, backtrack);
         }
 
@@ -114,7 +136,8 @@ namespace AStarFunctions
 
       }
         //Return an empty list if no path is possible
-        return new List<Tuple<int, int>>();
+        // Console.WriteLine(count);
+        return new List<Tuple<int,int>>();
     }
   }
 }
