99e3b984693f702882007ca22cd8f4b6aa2551c2
NukeyFox <johananmahendran@gmail.com>
2022-06-27 01:51:24 +0100

    Fixed TileSearch

--- Assets/Scripts/TileSearch.cs
+++ Assets/Scripts/TileSearch.cs
@@ -1,57 +1,58 @@
 using System.Collections.Generic;
+using System;
 
 namespace TileSearch{
 
 
   /*
     Usage:
-      public static int[] findTile(startx, int starty, T type, T[,] map);
+      public static Tuple<int,int> findTile(startx, int starty, T type, T[,] map);
       -- searches for the nearest tile of type "type" given a map starting from (startx, starty);
       -- returns a pair of numbers corresponding to the x and y position of that tile.
       
       Example usage:
       int[,] test_terrain = new int[,] { { 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 1}, { 0, 0, 0, 0, 0 }, {0, 0, 0, 0, 0}, {0, 0, 0, 1, 5}};
-      int[] nearest = TileSearcher<int>.findTile(0,0,5,test_terrain);
+      Tuple<int,int> nearest = TileSearcher<int>.findTile(0,0,5,test_terrain);
 
   */
   class TileSearcher<T>{
 
       //searches for the closest "type" tile starting from position (startx, starty), using BFS
-      public static int[] findTile(int startx, int starty, T type, T[,] map) {
-          Queue<int[]> queue = new Queue<int[]>();
-          HashSet<int[]> visited = new HashSet<int[]>();
-          int[] source = new int[] {startx, starty};
+      public static Tuple<int,int> findTile(int startx, int starty, T type, T[,] map) {
+          Queue<Tuple<int,int>> queue = new Queue<Tuple<int,int>>();
+          HashSet<Tuple<int,int>> visited = new HashSet<Tuple<int,int>>();
+          Tuple<int,int> source = new Tuple<int,int> (startx, starty);
           queue.Enqueue(source);
           visited.Add(source);
           int height = map.GetLength(0); 
           int width = map.GetLength(1); 
 
-          while (queue.Count > 0){
-              int[] node = queue.Dequeue();
-              int x = node[0];
-              int y = node[1]; 
+          while (queue.Count > 0 ){
+              Tuple<int,int> node = queue.Dequeue();
+              int x =node.Item1;
+              int y = node.Item2; 
               if (map[x,y].Equals(type)){return node;}
              
-              int[] left = new int[] {x-1, y};
-              if (x-1 >= 0 && !visited.Contains(left)){
+              Tuple<int,int> left = new Tuple<int,int> (x-1, y);
+              if (x-1 >= 0 && !(visited.Contains(left))){
                   visited.Add(left); queue.Enqueue(left);
               }
-              int[] right = new int[] {x+1, y};
-              if (x+1 < width && !visited.Contains(right)){
+              Tuple<int,int> right = new Tuple<int,int> (x+1, y);
+              if (x+1 < width && !(visited.Contains(right))){
                   visited.Add(right); queue.Enqueue(right);
               }
-              int[] up = new int[] {x, y-1};
-              if (y-1 >= 0 && !visited.Contains(up)){
+              Tuple<int,int> up = new Tuple<int,int> (x, y-1);
+              if (y-1 >= 0 && !(visited.Contains(up))){
                   visited.Add(up); queue.Enqueue(up);
               }
-              int[] down = new int[] {x, y+1};
-              if (y+1 < height && !visited.Contains(down)){
+              Tuple<int,int> down = new Tuple<int,int> (x, y+1);
+              if (y+1 < height && !(visited.Contains(down))){
                   visited.Add(down); queue.Enqueue(down);
               }
 
           }
 
-          return new int[0]; //return an empty array if no tile found. 
+          return null; //return an empty tuple if no tile found. 
       }
 
 
